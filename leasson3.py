ff2380f5e2d5e720cf032c00a04cc716c4b0693b
davidhs0516 <davidhs0516@gmail.com>
2023-04-14 21:40:11 +0800

    init commit

--- leasson3.py
+++ leasson3.py
@@ -1,8 +1,27 @@
 import private
 import requests
 
-url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttKey}?value1=31c&value2=52&value3=10'
+#url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.iftttKey}?value1=31c&value2=52&value3=10'
 
-r = requests.get(url)
-if r.status_code == 200:
-    print("發送成功")
+#r = requests.get(url)
+#if r.status_code == 200:
+#    print("發送成功")
+
+from gpiozero import Button
+from signal import pause
+
+state = False
+
+def user_press():
+    global state
+    state = not state
+    if state == True:
+        print("開燈")
+    else:
+        print("關燈")
+
+button = Button(18)
+
+button.when_pressed = user_press
+
+pause()
